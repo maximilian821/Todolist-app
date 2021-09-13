d41e10623e9060183686824a2cef4624312dc3f7
Maxim Glukhov <maximiliano18213@gmail.com>
2021-09-13 21:19:41 +0500

    add more routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index'
+  get 'app', to: 'home#app'
+  get 'users', to: redirect('/app')
+  resources :categories, path: 'app', except: [:index] do
+    resources :tasks, only: [:index]
+  end
+  scope :app do
+    resources :tasks, except: [:index]
+  end
 end
