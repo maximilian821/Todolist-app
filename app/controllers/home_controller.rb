0d036c27c6f7c631499bec70b7e7adf4e8c9a76f
Maxim Glukhov <maximiliano18213@gmail.com>
2021-09-14 23:28:47 +0500

    add ajax for categories

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,8 +6,19 @@ class HomeController < ApplicationController
 
   def app
     @task = Task.new
-    @tasks = Task.all
     @category = Category.new
     @categories = Category.all
+    if params[:category]
+      @category = Category.find_by(name: params[:category])
+      @tasks = @category.tasks.order(created_at: :desc)
+    elsif params[:id] == '0'
+      @tasks = Task.where(category_id: nil).order(created_at: :desc)
+    else
+    @tasks = Task.all.order(created_at: :desc)
+    end
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 end
